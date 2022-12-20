a3ce90befbd0f96f7b2d8db3454c9fa6ba7bcc0d
helau64 <heather@hactar.is>
2022-12-20 11:41:10 +0000

    Navigation and more basic styling

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,5 +1,7 @@
 import filterEvents from './components/events';
+import initNavigationToggle from './components/navigation';
 
 filterEvents();
+initNavigationToggle();
 
 document.querySelector('html').classList.remove('no-js');
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
       md: '768px',
       lg: '1024px',
       xl: '1280px'
+    },
+    fontFamily: {
+      amaranth: [
+        'Amaranth',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ]
     }
   },
   variants: {},
